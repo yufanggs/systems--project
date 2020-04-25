e01ab68ef5091d9908863eaf808a7391f52c9465
yufanggs <51052712+yufanggs@users.noreply.github.com>
2020-04-25 21:59:28 +0800

    Add files via upload

---
+++ 13) Experimental Runs/localscript.js
@@ -0,0 +1,57 @@
+/* Javascript for 16 Requirements Allocation */
+
+/* Functions for bd
+********************************************/
+function bd_in(){
+  var a = document.getElementsByClassName("bd");
+  var i;
+  for (i = 0; i < a.length; i++) {
+    a[i].style.background = "#fbed6a";
+  }
+}
+
+function bd_out(){
+  var a = document.getElementsByClassName("bd");
+  var i;
+  for (i = 0; i < a.length; i++) {
+    a[i].style.background = "#fefad6";
+  }
+}
+
+/* Function for ad
+********************************************/
+function ad_in(){
+  var a = document.getElementsByClassName("ad");
+  var i;
+  for (i = 0; i < a.length; i++) {
+    a[i].style.background = "#fbed6a";
+  }
+}
+
+function ad_out(){
+  var a = document.getElementsByClassName("ad");
+  var i;
+  for (i = 0; i < a.length; i++) {
+    a[i].style.background = "#fefad6";
+  }
+}
+
+/* Function for ld
+********************************************/
+function ld_in(){
+  var a = document.getElementsByClassName("ld");
+  var i;
+  for (i = 0; i < a.length; i++) {
+    a[i].style.background = "#fbed6a";
+  }
+}
+
+function ld_out(){
+  var a = document.getElementsByClassName("ld");
+  var i;
+  for (i = 0; i < a.length; i++) {
+    a[i].style.background = "#fefad6";
+  }
+}
+
+
